194c7d48648bda912909999f657a9b7f01becc16
5chr4mm <jeferson.schramm@gmail.com>
2016-12-06 18:26:56 -0200

    aha
    aham

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $db = mysql_connect("DATABASE1_HOST","DATABASE1_USER","DATABASE1_PASS");
 if (!$db) {
-    die('Não foi possível conectar: ' . mysql_error());
+die('Não foi possível conectar: ' . mysql_error());}
 $dado = mysql_select_db("DATABASE1_NAME",$db);
 ?>
