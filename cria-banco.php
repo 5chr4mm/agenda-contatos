6d7a411feb4bf44c9dfcf6824bf7f0bc3fb8810f
5chr4mm <jeferson.schramm@gmail.com>
2016-12-06 17:19:26 -0200

    Alterações BD
    Adicionado e removido alguns arquivos

--- cria-banco.php
+++ cria-banco.php
@@ -1,12 +1,26 @@
 <?php
-
-$restore_file  = "script-database.sql";
-$server_name   = "DATABASE1_HOST";
-$username      = "DATABASE1_USER";
-$password      = "DATABASE1_PASS";
-$database_name = "DATABASE1_NAME";
-
-$cmd = "mysql -h {$server_name} -u {$username} -p{$password} {$database_name} < $restore_file";
-exec($cmd);
-
+   $dbhost = 'DATABASE1_HOST';
+   $dbuser = 'DATABASE1_USER';
+   $dbpass = 'DATABASE1_PASS';
+   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+   
+   if(! $conn ) {
+      die('Não pode conectar: ' . mysql_error());
+   }
+   
+   $query_file = 'sql_query.txt';
+   
+   $fp = fopen($query_file, 'r');
+   $sql = fread($fp, filesize($query_file));
+   fclose($fp); 
+   
+   mysql_select_db('DATABASE1_NAME');
+   $retval = mysql_query( $sql, $conn );
+   
+   if(! $retval ) {
+      die('Não pode criar a tabela: ' . mysql_error());
+   }
+   
+   echo "Tabela agenda criada com sucesso\n";
+   mysql_close($conn);
 ?>
