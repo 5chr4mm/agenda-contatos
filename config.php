f022fd9a08041ee35740585f879bf1673ab009f9
5chr4mm <jeferson.schramm@gmail.com>
2016-12-07 07:34:58 -0200

    PHP
    Alterado os comandos para PHP 7

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
-$db = mysqli_connect("DATABASE1_HOST","DATABASE1_USER","DATABASE1_PASS");
+$db = mysqli_connect("DATABASE1_HOST","DATABASE1_USER","DATABASE1_PASS", "DATABASE1_NAME");
 if (!$db) {
-die('Não foi possível conectar: ' . mysqli_error());}
-$dado = mysqli_select_db("DATABASE1_NAME",$db);
+	echo "Não foi possível conectar." . PHP_EOL;
+	exit;
+}
 ?>
