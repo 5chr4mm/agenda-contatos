5a452585d37732e14ef64c99467fb4a00647032c
5chr4mm <jeferson.schramm@gmail.com>
2016-12-08 17:10:20 -0200

    DB
    Alterado

--- config.php
+++ config.php
@@ -6,5 +6,12 @@ define('DB_PASSWORD', $_SERVER['DATABASE1_PASS']);
 define('DB_NAME', $_SERVER['DATABASE1_NAME']);
 
 $db = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD);
-$dado = mysqli_select_db(DB_NAME,$db);
+if (!$db) {
+    die("Database connection failed: " . mysqli_error());
+}
+
+$dado = mysqli_select_db($db, DB_NAME);
+if (!$dado) {
+    die("Database selection failed: " . mysqli_error());
+}
 ?>
